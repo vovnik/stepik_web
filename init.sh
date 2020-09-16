fb0daa681e20203669915435a6457a57616001ba
Vladimir Ubuntu <vnikulin@eu.spb.ru>
2020-09-16 11:21:16 +0300

    commit with django 2.1 vers

--- init.sh
+++ init.sh
@@ -1,11 +1,3 @@
-sudo apt-get update
-sudo apt-get install -y python3.8
-sudo apt-get install -y python3.8-dev
-sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.8 /usr/bin/python3
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade django==3.1.1
-sudo pip3 install --upgrade gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart 
 sudo gunicorn -c hello.py hello:wsgi_application
