9329e0f769a3b919c9d860183a242381979cb242
Vladimir Ubuntu <vnikulin@eu.spb.ru>
2020-09-22 01:27:00 +0300

    mysql commit

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
-sudo mysql -uroot -e "create django_db"
+sudo /etc/init.d/mysql start
+sudo mysql -uroot -e "create database stepic_web;"
+sudo mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
 sudo python manage.py makemigrations 
+sudo python manage.py migrate
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart 
 sudo gunicorn -c hello.py hello:wsgi_application
