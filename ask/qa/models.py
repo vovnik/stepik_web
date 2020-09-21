1fb6c3fb223a60a1a557a2d943d9c658cd7ed09f
Vladimir Ubuntu <vnikulin@eu.spb.ru>
2020-09-22 01:20:49 +0300

    mysql commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from  django.contrib.auth.models import User  
 
 # Create your models here.
+class QuestionManager(models.Manager):                                          
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -11,9 +18,13 @@ class Question(models.Model):
     author = models.OneToOneField(User)
     likes = models.ManyToManyFields(User, related_name='question_like_user') 
     
+    objects = QuestionManager()
+    
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True) 
     question = models.OneToOne(Question)
     author = models.OneToOne(User) 
+    
+
