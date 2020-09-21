41d3c497aae0d6ef28d083401bfb3051ad612d45
Vladimir Ubuntu <vnikulin@eu.spb.ru>
2020-09-22 01:43:09 +0300

    mysql commit

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa.apps.QaConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stepic_web',
+        'NAME': 'stepik_web',
         'USER': 'box',
     }
 }
--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
+sudo pip3 install django==2.0.7
 sudo /etc/init.d/mysql start
-sudo mysql -uroot -e "create database stepic_web;"
-sudo mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
-sudo python manage.py makemigrations 
-sudo python manage.py migrate
+sudo mysql -uroot -e "create database stepik_web;"
+sudo mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
+sudo python3 /home/box/web/ask/manage.py makemigrations 
+sudo python3 /home/box/web/ask/manage.py migrate
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart 
-sudo gunicorn -c hello.py hello:wsgi_application
-cd /home/box/web/ask
-sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
+
