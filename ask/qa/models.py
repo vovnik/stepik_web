973977778f6bed4f3355de542910039d0e8a781b
Vladimir Ubuntu <vnikulin@eu.spb.ru>
2020-09-22 01:16:26 +0300

    mysql commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from  django.contrib.auth.models import User  
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True) 
+    rating = models.IntegerField(default = 0) 
+    author = models.OneToOneField(User)
+    likes = models.ManyToManyFields(User, related_name='question_like_user') 
+    
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True) 
+    question = models.OneToOne(Question)
+    author = models.OneToOne(User) 
--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
+sudo mysql -uroot -e "create django_db"
+sudo python manage.py makemigrations 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart 
 sudo gunicorn -c hello.py hello:wsgi_application
-sudo gunicorn -b 0.0.0.0:8000 ask.ask.wsgi:application
+cd /home/box/web/ask
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
